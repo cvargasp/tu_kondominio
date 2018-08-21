6e08000cd3288d8df1ec43fee3e5fdd06be4e1a8
cvargasp <cvargasp91@gmail.com>
2018-08-21 18:22:17 -0300

    fix email
    envió desde contacto@tukondominio.cl

--- app/Http/Controllers/contactMessageController.php
+++ app/Http/Controllers/contactMessageController.php
@@ -48,7 +48,7 @@ class ContactMessageController extends Controller
     	);
 
     	Mail::send('emails.contact-message', $data, function($message) use($data){
-    		$message->from('carlos.vargas@tukondominio.cl');
+    		$message->from('contacto@tukondominio.cl');
     		$message->replyto($data['email']);
     		$message->to('contacto@tukondominio.cl');
     		$message->subject($data['name']);
--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'carlos.vargas@tukondominio.cl'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
